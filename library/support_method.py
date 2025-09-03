0582cb06e62778426457f7fd6f0f2fa8b754aa67
Shabbir <91405793+shabbir2920@users.noreply.github.com>
2025-09-03 22:13:11 +0530

    Update support_method.py
    add changes to write password and email in text file

--- library/support_method.py
+++ library/support_method.py
@@ -6,6 +6,9 @@ def random_email_generator():
     using faker library"""
     dummy = Faker()
     dummy_email = dummy.email(safe=True, domain='gmail.com')
+    with open("credentials_username.txt", "w") as file:
+        file.write(f"the username is - {dummy_email}")
+        file.close()
     print(f"the email id of user is {dummy_email}")
     return dummy_email
 
@@ -15,5 +18,8 @@ def random_password_generator():
     using faker library"""
     dummy = Faker()
     dummy_password = dummy.password(length=10, special_chars=True, upper_case=True)
+    with open("credentials_password.txt", "w") as file:
+        file.write(f"the password is - {dummy_password}")
+        file.close()
     print(f"the password of user is {dummy_password}")
     return dummy_password
